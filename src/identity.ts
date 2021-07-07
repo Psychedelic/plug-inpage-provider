e62a531c7bd66e3cd9606484aa9f81ac6bc6173d
jsonsivar <jsonsivar@gmail.com>
2021-07-07 11:46:17 -0400

    WIP code for using sendMessage

--- src/identity.ts
+++ src/identity.ts
@@ -3,6 +3,22 @@ import { SignIdentity, PublicKey, BinaryBlob, DerEncodedBlob } from '@dfinity/ag
 
 type SignCb = (payload: ArrayBuffer) => Promise<ArrayBuffer>;
 
+const extensionId = 'NEED_TO_SET_THIS';
+
+const sendMessage = (args, callback) => {
+  chrome.runtime.sendMessage(extensionId, args, (response) => {
+    let parsedResponse = response;
+    if (typeof response === 'string') {
+      try {
+        parsedResponse = JSON.parse(response);
+      } catch (error) {
+        parsedResponse = response;
+      }
+    }
+    callback(parsedResponse);
+  });
+};
+
 export class PlugIdentity extends SignIdentity {
   constructor(private publicKey: DerEncodedBlob, private signCb: SignCb) {
     super();
@@ -16,7 +32,10 @@ export class PlugIdentity extends SignIdentity {
 
   async sign(blob: BinaryBlob): Promise<BinaryBlob> {
     const ab = blob.buffer.slice(blob.byteOffset, blob.byteOffset + blob.byteLength);
-    const res = await this.signCb(ab);
+    let res;
+    sendMessage(ab, async (d)=> {
+      res = await this.signCb(d);
+    });
     return Buffer.from(res) as BinaryBlob;
   }
 }
