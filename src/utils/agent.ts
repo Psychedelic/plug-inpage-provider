10e2e963e2eede458e67a56b703af8289e21ed83
aleacevedo <ale.acevedo@live.com.ar>
2021-10-18 16:17:31 -0300

    refactor: move agent method to utils

---
+++ src/utils/agent.ts
@@ -0,0 +1,63 @@
+import { HttpAgent, Actor, ActorSubclass } from "@dfinity/agent";
+
+import { PlugIdentity } from "../identity";
+import { signFactory } from "./sign";
+
+export interface CreateAgentParams {
+  whitelist?: string[];
+  host?: string;
+}
+
+interface CreateAgentParamsFixed {
+  whitelist: string[];
+  host: string;
+}
+
+const DEFAULT_HOST = "https://mainnet.dfinity.network";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const DEFAULT_CREATE_AGENT_ARGS: CreateAgentParamsFixed = {
+  whitelist: [],
+  host: DEFAULT_HOST,
+};
+
+export const createAgent = async (
+  clientRPC,
+  metadata,
+  {
+    whitelist = DEFAULT_CREATE_AGENT_ARGS.whitelist,
+    host = DEFAULT_CREATE_AGENT_ARGS.host,
+  }: CreateAgentParams,
+  idls,
+  preAprove = false
+) => {
+  const publicKey = await clientRPC.callClientRPC({
+    handler: "verifyWhitelist",
+    args: [metadata, whitelist],
+    config: {
+      timeout: 0,
+      target: "",
+    },
+  });
+
+  const identity = new PlugIdentity(
+    publicKey,
+    signFactory(clientRPC, idls, preAprove),
+    whitelist
+  );
+
+  return new HttpAgent({
+    identity,
+    host,
+  });
+};
+
+export const createActor = async <T>(
+  agent,
+  canisterId,
+  interfaceFactory
+): Promise<ActorSubclass<T>> => {
+  return Actor.createActor(interfaceFactory, {
+    agent: agent,
+    canisterId,
+  });
+};
