0ffd55e8e607037f0baf3295a1778058525f5711
Maksym Boytsov <maksym.boytsov@gmail.com>
2021-12-30 18:46:06 +0100

    fix: plug version

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-import packageJSON from '../package.json';
+import packageJSON from "../package.json";
 
 export const versions = {
-    extension: '0.4.1',
-    provider: packageJSON.version
-}
+  extension: "0.4.5",
+  provider: packageJSON.version,
+};
