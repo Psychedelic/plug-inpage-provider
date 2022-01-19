768aeef2d7c7c7430a30baafd53f5adceb324799
rocky-fleek <tomas@fleek.co>
2022-01-19 12:35:01 +0100

    Abstracted urls to a contant

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 import packageJSON from "../package.json";
 
+export const IC_MAINNET_URLS = ['https://mainnet.dfinity.network', 'ic0.app'];
+
 export const versions = {
   extension: "0.4.5",
   provider: packageJSON.version,
