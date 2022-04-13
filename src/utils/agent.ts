b1b5ad1295d2145ce983a96a59f46d88ab49f981
aleacevedo <ale.acevedo@live.com.ar>
2022-04-13 12:10:26 -0300

    feat: implement plug agent

--- src/utils/agent.ts
+++ src/utils/agent.ts
@@ -1,7 +1,27 @@
-import { HttpAgent, Actor, ActorSubclass } from "@dfinity/agent";
+import {
+  HttpAgent,
+  Actor,
+  ActorSubclass,
+  HttpAgentOptions,
+  QueryFields,
+  Identity,
+  QueryResponse,
+  QueryResponseStatus,
+  SubmitResponse,
+  ReadStateOptions,
+  ReadStateResponse,
+} from "@dfinity/agent";
+import {
+  BinaryBlob,
+  blobFromUint8Array,
+  blobToUint8Array,
+} from "@dfinity/candid";
+import { Principal } from "@dfinity/principal";
 import { IC_MAINNET_URLS } from "../constants";
 
 import { PlugIdentity } from "../identity";
+import RPCManager from "../modules/RPCManager";
+import { base64ToBuffer, bufferToBase64 } from "./communication";
 import { signFactory } from "./sign";
 
 export interface CreateAgentParams {
@@ -21,22 +41,139 @@ const DEFAULT_CREATE_AGENT_ARGS: CreateAgentParamsFixed = {
   host: DEFAULT_HOST,
 };
 
-export const privateCreateAgent = async ({ publicKey, clientRPC, idls, preApprove = false, whitelist = DEFAULT_CREATE_AGENT_ARGS.whitelist, host = DEFAULT_CREATE_AGENT_ARGS.host }) => {
+const callMethodFactory =
+  (clientRPC: RPCManager) =>
+  async (
+    canisterId: Principal | string,
+    options: {
+      methodName: string;
+      arg: BinaryBlob;
+      effectiveCanisterId?: Principal | string;
+    },
+    identity?: Identity | Promise<Identity>
+  ): Promise<SubmitResponse> => {
+    const canisterIdStr =
+      typeof canisterId === "string" ? canisterId : canisterId.toString();
+    const effectiveCanisterIdStr =
+      typeof options.effectiveCanisterId === "string"
+        ? options.effectiveCanisterId
+        : options.effectiveCanisterId?.toString();
+    const result = await clientRPC.call({
+      handler: "requestCall",
+      args: {
+        canisterId: canisterIdStr,
+        methodName: options.methodName,
+        arg: bufferToBase64(Buffer.from(blobToUint8Array(options.arg).buffer)),
+        effectiveCanisterId: effectiveCanisterIdStr,
+      },
+    });
+    if (result.error) throw result.error.message;
+
+    return {
+      ...result.response,
+      requestId: blobFromUint8Array(
+        new Uint8Array(base64ToBuffer(result.response.requestId))
+      ),
+    };
+  };
+
+const queryMethodFactory =
+  (clientRPC: RPCManager) =>
+  async (
+    canisterId: Principal | string,
+    fields: QueryFields,
+    identity?: Identity | Promise<Identity>
+  ): Promise<QueryResponse> => {
+    const canisterIdStr =
+      typeof canisterId === "string" ? canisterId : canisterId.toString();
+    const result = await clientRPC.call({
+      handler: "requestQuery",
+      args: {
+        canisterId: canisterIdStr,
+        methodName: fields.methodName,
+        arg: bufferToBase64(Buffer.from(blobToUint8Array(fields.arg).buffer)),
+      },
+    });
+    if (result.error) throw result.error.message;
+
+    return result.response.status === QueryResponseStatus.Replied
+      ? {
+          status: QueryResponseStatus.Replied,
+          reply: {
+            arg: blobFromUint8Array(
+              new Uint8Array(base64ToBuffer(result.response.status))
+            ),
+          },
+        }
+      : {
+          ...result.response,
+          status: QueryResponseStatus.Rejected,
+        };
+  };
+
+const readStateMethodFactory =
+  (clientRPC: RPCManager) =>
+  async (
+    canisterId: Principal | string,
+    fields: ReadStateOptions,
+    identity?: Identity | Promise<Identity>
+  ): Promise<ReadStateResponse> => {
+    const canisterIdStr =
+      typeof canisterId === "string" ? canisterId : canisterId.toString();
+    const result = await clientRPC.call({
+      handler: "requestReadState",
+      args: {
+        canisterId: canisterIdStr,
+        paths: fields.paths[0].map((path) =>
+          bufferToBase64(Buffer.from(blobToUint8Array(path).buffer))
+        ),
+      },
+    });
+    if (result.error) throw result.error.message;
+
+    return {
+      certificate: blobFromUint8Array(
+        new Uint8Array(base64ToBuffer(result.response.status))
+      ),
+    };
+  };
+
+class PlugAgent extends HttpAgent {
+  constructor(options: HttpAgentOptions = {}, clientRPC: RPCManager) {
+    super(options);
+
+    this["query"] = queryMethodFactory(clientRPC);
+    this["call"] = callMethodFactory(clientRPC);
+    this["readState"] = readStateMethodFactory(clientRPC);
+  }
+}
+
+export const privateCreateAgent = async ({
+  publicKey,
+  clientRPC,
+  idls,
+  preApprove = false,
+  whitelist = DEFAULT_CREATE_AGENT_ARGS.whitelist,
+  host = DEFAULT_CREATE_AGENT_ARGS.host,
+}) => {
   const identity = new PlugIdentity(
     publicKey,
     signFactory(clientRPC, idls, preApprove),
     whitelist
   );
 
-  const agent = new HttpAgent({
-    identity,
-    host,
-  });
+  const agent = new PlugAgent(
+    {
+      identity,
+      host,
+    },
+    clientRPC
+  );
   if (!IC_MAINNET_URLS.includes(host)) {
     await agent.fetchRootKey();
   }
   return agent;
-}
+};
 
 export const createAgent = async (
   clientRPC,
---
+++ src/utils/communication.ts
@@ -0,0 +1,7 @@
+export const bufferToBase64 = (buf: Buffer): string => {
+  return Buffer.from(buf).toString("base64");
+};
+
+export const base64ToBuffer = (base64: string): Buffer => {
+  return Buffer.from(base64, "base64");
+};
