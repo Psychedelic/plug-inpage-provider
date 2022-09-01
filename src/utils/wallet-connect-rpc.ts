c58086e3b5545030877d845b98318664e4ddf7cb
aleacevedo <ale.acevedo@live.com.ar>
2022-09-01 12:38:55 -0300

    fix: dont check isUnlock before calls

--- src/utils/wallet-connect-rpc.ts
+++ src/utils/wallet-connect-rpc.ts
@@ -8,7 +8,6 @@ import {
   SIGN_METHODS,
   DEFAULT_TIMEOUT,
   WC_MOBILE_REGISTRY_ENTRY,
-  IS_UNLOCK_METHOD,
   IS_ALL_WHITELISTED_METHOD,
 } from "../constants/wallet-connect";
 import { SimplifiedRPC } from "../Provider/interfaces";
@@ -24,14 +23,16 @@ class WalletConnectRPC implements SimplifiedRPC {
   focusUri: any;
   agent: HttpAgent | null = null;
   isAndroid: boolean = false;
+  debug: boolean;
 
-  constructor(window: Window) {
+  constructor(window: Window, debug = true) {
     this.isAndroid = isAndroid();
     this.wcClient = new WalletConnect({
       bridge: this.wcBridgeURL,
       signingMethods: SIGN_METHODS,
     });
     this.window = window;
+    this.debug = debug;
   }
 
   public start() {}
@@ -72,31 +73,27 @@ class WalletConnectRPC implements SimplifiedRPC {
   }
 
   private _call(handler, args, resolve, reject) {
+    this.debug && console.log("_calling isUnlock", handler);
+    // this.debug && console.log("_called isUnlock", handler, isUnlock);
+    const requestId = payloadId();
+    this.debug && console.log("_calling", handler);
     this.wcClient
       .sendCustomRequest({
-        method: IS_UNLOCK_METHOD,
+        id: requestId,
+        method: handler,
+        params: args,
       })
-      .then((isUnlock) => {
-        const requestId = payloadId();
-        this.wcClient
-          .sendCustomRequest({
-            id: requestId,
-            method: handler,
-            params: args,
-          })
-          .then((response) => {
-            resolve(response);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-        if (!isUnlock || SIGN_METHODS.includes(handler)) {
-          this.window.location.href = `${this.focusUri}?requestId=${requestId}`;
-        }
+      .then((response) => {
+        this.debug && console.log("_called", handler, response);
+        resolve(response);
       })
       .catch((error) => {
+        this.debug && console.log("_called", handler, error);
         reject(error);
       });
+    if (SIGN_METHODS.includes(handler)) {
+      this.window.location.href = `${this.focusUri}?requestId=${requestId}`;
+    }
   }
 
   private async requestConnect(args, resolve, _reject) {
@@ -132,70 +129,67 @@ class WalletConnectRPC implements SimplifiedRPC {
   private async requestCall(args, resolve, reject) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_metadata, _args, batchTxId] = args;
-
+    this.debug && console.log("requestingCall isUnlock");
+    // this.debug && console.log("requestedCall isUnlock", isUnlock);
+    const requestId = payloadId();
+    this.debug && console.log("requestingCall");
     this.wcClient
       .sendCustomRequest({
-        method: IS_UNLOCK_METHOD,
+        id: requestId,
+        method: "requestCall",
+        params: args,
       })
-      .then((isUnlock) => {
-        const requestId = payloadId();
-        this.wcClient
-          .sendCustomRequest({
-            id: requestId,
-            method: "requestCall",
-            params: args,
-          })
-          .then((response) => {
-            resolve(response);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-        if (!batchTxId || !isUnlock) {
-          this.window.location.href = `${this.focusUri}?requestId=${requestId}`;
-        }
+      .then((response) => {
+        this.debug && console.log("requestedCall", response);
+        resolve(response);
+      })
+      .catch((error) => {
+        this.debug && console.log("requestedCall", error);
+        reject(error);
       });
+    if (!batchTxId) {
+      this.window.location.href = `${this.focusUri}?requestId=${requestId}`;
+    }
   }
 
   private async verifyWhitelist(args, resolve, reject) {
+    this.debug && console.log("verifyingWhitelist isUnlock");
+    // this.debug && console.log("verifyedWhitelist isUnlock", isUnlock);
+    const whitelistedRequestId = payloadId();
+    this.debug && console.log("verifyingWhitelist allWhitelisted");
     this.wcClient
       .sendCustomRequest({
-        method: IS_UNLOCK_METHOD,
+        id: whitelistedRequestId,
+        method: IS_ALL_WHITELISTED_METHOD,
+        params: args,
       })
-      .then((isUnlock) => {
-        const whitelistedRequestId = payloadId();
-        this.wcClient
-          .sendCustomRequest({
-            id: whitelistedRequestId,
-            method: IS_ALL_WHITELISTED_METHOD,
-            params: args,
-          })
-          .then((allWhiteListed) => {
-            const verifyRequestId = payloadId();
-            this.wcClient
-              .sendCustomRequest({
-                id: verifyRequestId,
-                method: "verifyWhitelist",
-                params: args,
-              })
-              .then((response) => {
-                resolve(response);
-              })
-              .catch((error) => {
-                reject(error);
-              });
-            if (!allWhiteListed) {
-              this.window.location.href = `${this.focusUri}?requestId=${verifyRequestId}`;
-            }
-          })
-          .catch((error) => {
-            reject(error);
-          });
-        if (!isUnlock) {
-          this.window.location.href = `${this.focusUri}?requestId=${whitelistedRequestId}`;
+      .then((allWhiteListed) => {
+        this.debug &&
+          console.log("verifyingWhitelist allWhitelisted", allWhiteListed);
+        if (!allWhiteListed) {
+          const verifyRequestId = payloadId();
+          this.debug && console.log("verifyingWhitelist");
+          this.wcClient
+            .sendCustomRequest({
+              id: verifyRequestId,
+              method: "verifyWhitelist",
+              params: args,
+            })
+            .then((response) => {
+              this.debug && console.log("verifyedWhitelist", response);
+              resolve(response);
+            })
+            .catch((error) => {
+              this.debug && console.log("verifyedWhitelist", error);
+              reject(error);
+            });
+          this.window.location.href = `${this.focusUri}?requestId=${verifyRequestId}`;
+        } else {
+          resolve(allWhiteListed);
         }
       })
       .catch((error) => {
+        this.debug && console.log("verifyingWhitelist allWhitelisted", error);
         reject(error);
       });
   }
