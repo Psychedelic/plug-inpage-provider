47543f929e3216726f2be682448f6ba6c8b9422b
aleacevedo <ale.acevedo@live.com.ar>
2022-10-24 15:20:40 -0300

    chore: bump major version with wallet connect feature

--- src/utils/signer-server.ts
+++ src/utils/signer-server.ts
@@ -1,4 +1,4 @@
-const SIGNER_SERVER_URL = "https://signer-server.0xaace.xyz";
+const SIGNER_SERVER_URL = "https://signer-server.plugwallet.ooo";
 
 const requestCall = async (args, resolve, reject) => {
   const response = await fetch(`${SIGNER_SERVER_URL}/call`, {
